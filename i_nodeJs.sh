587eebe7be4dc892b724b0d9fd1d1527b1af3d9a
Miguel <miguel@opi.la>
2014-02-04 00:09:29 -0600

    agrego nodeJS

---
+++ i_nodeJs.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+yes | sudo apt-get install python-software-properties python g++ make
+echo | sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+yes |sudo  apt-get install nodejs
