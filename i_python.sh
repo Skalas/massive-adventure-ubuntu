845e8b15a98713783c35080d8c73dae130acd4f2
Miguel <miguel@opi.la>
2014-02-04 00:05:27 -0600

    Add files to install in python I'm missing the postgresql before this file

---
+++ i_python.sh
@@ -0,0 +1,15 @@
+yes |sudo apt-get install python-pip python-dev build-essential
+sudo pip install --upgrade pip
+sudo pip install --upgrade virtualenv 
+sudo pip install numpy
+sudo pip install pandas
+
+yes | sudo apt-get install libboost-all-dev
+sudo pip install jinja2
+sudo pip install sphinx
+sudo pip install pyzmq
+sudo pip install tornado
+sudo pip install pygments
+sudo pip install nose
+sudo easy_install psycopg2
+sudo easy_install pymongo
