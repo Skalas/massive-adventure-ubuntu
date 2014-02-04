484b70e46a8ae89ab0445cf06ce6ba803a2251c8
Miguel <miguel@opi.la>
2014-02-04 00:12:02 -0600

    Add R

---
+++ i_R.sh
@@ -0,0 +1,8 @@
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+###################### OJO #################### 
+##### Si se cambia la versión de ubuntu hay que cambiar esta línea.
+#####################################################
+sudo chmod ugo+rw /etc/apt/sources.list
+echo 'deb http://cran.r-project.org/bin/linux/ubuntu saucy/' >> '/etc/apt/sources.list'
+apt-get update
+yes | sudo apt-get install r-base r-base-dev
