9779b7dd35503a52504e8780a7d71497281ab34d
skalas <miguel@opi.la>
2014-03-20 19:45:22 -0600

    mejor manera

--- i_R_compiled.sh
+++ i_R_compiled.sh
@@ -3,9 +3,9 @@ cd
 wget -c http://cran.r-project.org/src/base/R-latest.tar.gz
 sudo mv /usr/lib/libf2c.so /usr/lib/libf2c.so_backup
 sudo ln -s /usr/lib/libf2c.a /usr/lib/libf2c.so
-mkdir R-latest
+
 tar zxvf R-latest.tar.gz
-cd "$(ls -dt R-3*/ | head -1 )"
+cd "$(ls -dt R-*/ | head -1 )"
 ./configure --enable-memory-profiling --enable-R-shlib --with-blas --with-lapack --with-system-zlib --with-system-bzlib --with-system-xz --with-tcltk  --with-cairo --with-libpng --with-jpeglib --with-libtiff
 make
 sudo 	make install
