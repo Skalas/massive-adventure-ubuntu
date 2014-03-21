49e80389e6e5fe0da1b687d6237c1db78cb100e8
skalas <miguel@opi.la>
2014-03-20 19:40:01 -0600

    cambio head por sed, y cambio lo de que no se pueden compilar c y fortran al mismo tiempo

--- i_R_compiled.sh
+++ i_R_compiled.sh
@@ -1,9 +1,11 @@
 sudo apt-get install build-essential libpq-dev liblapack3 libblas3 libmysql++-dev sqlite3 fort77 xorg-dev texinfo liblapack-dev liblapack3gf
 cd
 wget -c http://cran.r-project.org/src/base/R-latest.tar.gz
+sudo mv /usr/lib/libf2c.so /usr/lib/libf2c.so_backup
+sudo ln -s /usr/lib/libf2c.a /usr/lib/libf2c.so
 mkdir R-latest
 tar zxvf R-latest.tar.gz
-cd "$(ls -dt */ | head -1)"
+cd "$(ls -dt */ | sed 1d)"
 ./configure --enable-memory-profiling --enable-R-shlib --with-blas --with-lapack --with-system-zlib --with-system-bzlib --with-system-xz --with-tcltk  --with-cairo --with-libpng --with-jpeglib --with-libtiff
 make
 sudo 	make install
