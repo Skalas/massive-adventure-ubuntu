ef25bb7c09365b1ab75453f28dd292c4a8311e9d
skalas <miguel@opi.la>
2014-04-24 22:38:12 -0500

    install all in the same line

--- i_python.sh
+++ i_python.sh
@@ -1,15 +1,11 @@
-yes |sudo apt-get install python-pip python-dev build-essential
+yes |sudo apt-get install python-pip python-dev build-essential libboost-all-dev ipython-notebook python-tk
 sudo pip install --upgrade pip
-sudo pip install --upgrade virtualenv 
+sudo pip install --upgrade virtualenv
 sudo pip install numpy
 sudo pip install pandas
-
-yes | sudo apt-get install libboost-all-dev
 sudo pip install jinja2
 sudo pip install sphinx
 sudo pip install pyzmq
 sudo pip install tornado
 sudo pip install pygments
 sudo pip install nose
-
-yes | sudo apt-get install ipython-notebook python-tk
