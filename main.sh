e29115214b420bb241d199e8adc7c11f3e42bc2c
skalas <miguel@opi.la>
2014-04-24 23:41:48 -0500

    changes to the main

--- main.sh
+++ main.sh
@@ -1,6 +1,8 @@
 ## File that loads everything else, in modules. 
+sudo locale-gen es_MX.UTF-8
+./i_oh_my_zsh.sh
 ./i_python.sh
-./i_R.sh
-
+./i_R_compiled.sh
 ./i_mongo.sh
 ./i_postgres.sh
+./i_octave_compiled.sh
