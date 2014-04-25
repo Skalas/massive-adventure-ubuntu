9e505787e712a9292a6861eaf7b531850c15f917
skalas <miguel@opi.la>
2014-04-24 23:36:16 -0500

    adding installation files for mongodb

---
+++ i_mongo.sh
@@ -0,0 +1,4 @@
+echo 'y' | sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+sudo echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/sudo apt/sources.list.d/mongodb.list
+echo 'y' | sudo apt-get update
+echo 'y' | sudo apt-get install mongodb-10gen
