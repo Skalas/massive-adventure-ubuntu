45af3feaf2ac9c2ded2a4bd8981a524241556a2e
skalas <miguel@opi.la>
2014-04-24 23:36:37 -0500

    adding the installation for postgresql

---
+++ i_postgres.sh
@@ -0,0 +1,5 @@
+echo 'deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main' | sudo tee /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo 'y' | sudo apt-get update
+echo 'y' | sudo apt-get upgrade
+echo 'y' | sudo apt-get install postgresql-9.3 pgadmin3
