5083daa431a2b5cca989c827a187ca6966ee9319
skalas <miguel@opi.la>
2014-04-24 22:56:27 -0500

    doy el espacio necesario

--- i_python.sh
+++ i_python.sh
@@ -1,4 +1,4 @@
-yes |sudo apt-get install python-pip python-dev build-essential libboost-all-dev ipython-notebook python-tk
+yes | sudo apt-get install python-pip python-dev build-essential libboost-all-dev ipython-notebook python-tk
 sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
 sudo pip install numpy
