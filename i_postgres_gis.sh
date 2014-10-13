f62403f308943f7a45eef624bbcbb3819e922170
skalas <miguel@opi.la>
2014-10-12 20:18:50 -0500

    cambios al archivo instalador de postgres

--- i_postgres_gis.sh
+++ i_postgres_gis.sh
@@ -2,20 +2,20 @@
 
 # Postgres 9.3
 
-sudo apt-get update
-sudo apt-get -y install python-software-properties
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" >> /etc/apt/sources.list.d/postgresql.list'
-sudo apt-get update
-sudo apt-get install postgresql-9.3 libpq5 postgresql-contrib
+# sudo apt-get update
+# sudo apt-get -y install python-software-properties
+# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" >> /etc/apt/sources.list.d/postgresql.list'
+# sudo apt-get update
+# sudo apt-get install postgresql-9.3 libpq5 postgresql-contrib
 
 # Buena practica: detener cluster y nombrar nuevo
 
-cd
-sudo pg_dropcluster --stop 9.3 main
-sudo mkdir /data
-sudo chown -R postgres:postgres /data
-sudo pg_createcluster -d /data 9.3 main
+# cd
+# sudo pg_dropcluster --stop 9.3 main
+# sudo mkdir /data
+# sudo chown -R postgres:postgres /data
+# sudo pg_createcluster -d /data 9.3 main
 
 # Postgis
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
