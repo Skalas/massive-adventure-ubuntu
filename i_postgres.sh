2f993b59ba7a865abb69065a056049c2a12108c5
skalas <miguel@opi.la>
2014-10-23 17:48:35 -0500

    upgradeo el postgres a 9.4, y ubuntu a unicorn

--- i_postgres.sh
+++ i_postgres.sh
@@ -1,5 +1,5 @@
-echo 'deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main' | sudo tee /etc/apt/sources.list.d/pgdg.list
+echo 'deb http://apt.postgresql.org/pub/repos/apt/ utopic-pgdg main' | sudo tee /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 echo 'y' | sudo apt-get update
 echo 'y' | sudo apt-get upgrade
-echo 'y' | sudo apt-get install postgresql-9.3
+echo 'y' | sudo apt-get install postgresql-9.4
