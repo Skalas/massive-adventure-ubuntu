36b195aad4198ca1b9cd74fba931fe0c9e520ed1
skalas <miguel@opi.la>
2014-10-23 19:12:47 -0500

    Agrego installador para gnu Parallel

---
+++ i_gnuParallel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd 
+wget http://ftp.gnu.org/gnu/parallel/parallel-latest.tar.bz2
+tar -xvf parallel-latest.tar.bz2
+cd "$(ls -dt parallel-*/ | head -1 )"
+./configure
+make
+sudo make install
