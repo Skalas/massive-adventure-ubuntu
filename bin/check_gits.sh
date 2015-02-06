9083c4495bbd99592859f173d99dcc846bdd5923
skalas <miguel@opi.la>
2015-02-06 14:46:45 -0600

    Agrego el archivo para los gits

---
+++ bin/check_gits.sh
@@ -0,0 +1,12 @@
+cd $1
+red='\033[0;33m'
+NC='\033[0m' # No Color
+for i in `ls`
+do
+    cd $i
+    echo "================================"
+    echo "== Directory: ${red}$i${NC}"
+    echo "================================"
+    git status
+    cd ..
+done
