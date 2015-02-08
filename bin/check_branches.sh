248f771a364c4c75d686e2c75273a221523493db
skalas <miguel@opi.la>
2015-02-08 16:41:28 -0600

    ahora este lista todos los branches

---
+++ bin/check_branches.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cd $1
+red='\033[0;33m'
+NC='\033[0m' # No Color
+for i in `ls`
+do
+    cd $i
+    echo "================================"
+    echo "== Directory: ${red}$i${NC}"
+    echo "================================"
+    git branch -a
+    cd ..
+done
