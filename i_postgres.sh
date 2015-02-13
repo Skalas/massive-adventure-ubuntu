1d7ebecb7dadd709f7e0ce278d91be286f2aaff1
skalas <miguel@opi.la>
2015-02-13 01:14:16 -0600

    Agrego la creación del usuario y de la base de datos correspondiente para poder usar postgres de jalón

--- i_postgres.sh
+++ i_postgres.sh
@@ -1,5 +1,11 @@
 echo 'deb http://apt.postgresql.org/pub/repos/apt/ utopic-pgdg main' | sudo tee /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-echo 'y' | sudo apt-get update
-echo 'y' | sudo apt-get upgrade
-echo 'y' | sudo apt-get install postgresql-9.4
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install postgresql-9.4
+sudo -u postgres bash <<EOF
+createuser "$USERER"
+createdb "$USER"
+psql -c "alter role $USER with login superuser;"
+psql -c "alter database $USER  owner to $USER;"
+EOF
