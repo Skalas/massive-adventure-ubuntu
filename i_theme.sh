5b85cfcac1c97ef6c621a65776df7b4ee2888e1f
skalas <miguel@opi.la>
2015-02-13 01:23:39 -0600

    Agrego font

--- i_theme.sh
+++ i_theme.sh
@@ -4,7 +4,9 @@ sudo add-apt-repository -y ppa:noobslab/icons
 sudo apt-get update
 sudo apt-get install ultra-flat-icons
 sudo apt-get install royal-gtk-theme
-## Las fonts
+## Usar sourcecodePro como font
 [ -d /usr/share/fonts/opentype ] || sudo mkdir /usr/share/fonts/opentype
 sudo git clone https://github.com/adobe-fonts/source-code-pro.git /usr/share/fonts/opentype/scp
 sudo fc-cache -f -v
+## Uso base16 ocean dark.
+source ./base16-gnome-terminal/base16-ocean.dark.sh
