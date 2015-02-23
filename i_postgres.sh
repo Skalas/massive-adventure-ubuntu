fde3f18bab4262755320a237a9267b5683595917
skalas <miguel@opi.la>
2015-02-23 17:36:13 -0600

    Había un bug con el usuario, no me había dado cuenta...

--- i_postgres.sh
+++ i_postgres.sh
@@ -1,11 +1,14 @@
-echo 'deb http://apt.postgresql.org/pub/repos/apt/ utopic-pgdg main' | sudo tee /etc/apt/sources.list.d/pgdg.list
+#!/bin/bash
+ubuntu_codename=`lsb_release -cs` 
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $ubuntu_codename-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install postgresql-9.4
+usr=$USER
 sudo -u postgres bash <<EOF
-createuser "$USERER"
-createdb "$USER"
-psql -c "alter role $USER with login superuser;"
-psql -c "alter database $USER  owner to $USER;"
+createuser "$usr"
+createdb "$usr"
+psql -c "alter role $usr with login superuser;"
+psql -c "alter database $usr  owner to $usr;"
 EOF
