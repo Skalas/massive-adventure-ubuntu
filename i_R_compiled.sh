111190a505f8e662b2f01e31e2c708d6ba2a7e45
skalas <miguel@opi.la>
2015-03-19 18:53:32 -0600

    Quito los 'echos'

--- i_R_compiled.sh
+++ i_R_compiled.sh
@@ -1,11 +1,9 @@
 sudo apt-get update
-echo 'y' | sudo apt-get upgrade
-echo 'y' | sudo apt-get install build-essential libpq-dev liblapack3 libblas3 libmysql++-dev sqlite3 fort77 gnuplot-x11 gfortran  texinfo liblapack-dev liblapack3gf texi2html libglpk-dev libgeos-dev libgdal1-dev libproj-dev
-echo 'y' | sudo apt-get build-dep r-base
+sudo apt-get upgrade -y
+sudo apt-get install -y build-essential libpq-dev liblapack3 libblas3 libmysql++-dev sqlite3 fort77 gnuplot-x11 gfortran  texinfo liblapack-dev liblapack3gf texi2html libglpk-dev libgeos-dev libgdal1-dev libproj-dev
+sudo apt-get -y build-dep r-base 
 cd
 wget -c http://cran.r-project.org/src/base/R-latest.tar.gz
-#sudo mv /usr/lib/libf2c.so /usr/lib/libf2c.so_backup
-#sudo ln -s /usr/lib/libf2c.a /usr/lib/libf2c.so
 
 tar zxvf R-latest.tar.gz
 cd "$(ls -dt R-*/ | head -1 )"
