8380bfc460ef02f5eadc5eb69306d4478d70da83
skalas <miguel@opi.la>
2015-04-20 15:59:54 -0500

    quito las cosas que no necesariamente ocupaba

--- i_python.sh
+++ i_python.sh
@@ -3,9 +3,3 @@ sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
 sudo pip install numpy
 sudo pip install pandas
-sudo pip install jinja2
-sudo pip install sphinx
-sudo pip install pyzmq
-sudo pip install tornado
-sudo pip install pygments
-sudo pip install nose
