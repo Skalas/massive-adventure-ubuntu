fbada3b920532f434436146ea594c189b98d32db
skalas <miguel@opi.la>
2015-04-21 18:44:38 -0500

    actualizo la versión de node

--- i_nvm.sh
+++ i_nvm.sh
@@ -2,5 +2,5 @@
 curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
 tail -3 ~/.bashrc >> ~/.zshrc
 source ~/.zshrc
-nvm install v0.10.33
-nvm alias default v0.10.33
+nvm install v0.12.2
+nvm alias default v0.12.2
