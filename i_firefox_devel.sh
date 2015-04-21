346157e5c9cb674ef7e954f783b8934603c03fb5
skalas <miguel@opi.la>
2015-04-21 18:43:26 -0500

    Agrego los -y que me di cuenta que faltaban

--- i_firefox_devel.sh
+++ i_firefox_devel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora
+sudo add-apt-repository -y ppa:ubuntu-mozilla-daily/firefox-aurora
 sudo apt-get update
-sudo apt-get install firefox
+sudo apt-get install -y firefox
--- i_theme.sh
+++ i_theme.sh
@@ -2,8 +2,8 @@
 sudo add-apt-repository -y ppa:noobslab/themes
 sudo add-apt-repository -y ppa:noobslab/icons
 sudo apt-get update
-sudo apt-get install ultra-flat-icons
-sudo apt-get install royal-gtk-theme
+sudo apt-get install -y ultra-flat-icons
+sudo apt-get install -y royal-gtk-theme
 ## Usar sourcecodePro como font
 [ -d /usr/share/fonts/opentype ] || sudo mkdir /usr/share/fonts/opentype
 sudo git clone https://github.com/adobe-fonts/source-code-pro.git /usr/share/fonts/opentype/scp
