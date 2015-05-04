7068476bb5f82222a491e7880003f3117e75feed
skalas <miguel@opi.la>
2015-05-04 15:30:10 -0500

    Agrego instalación de julia.

---
+++ i_julia.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+while true; do
+    read -p "Do you wish to Compile Julia? y/n " yn
+    case $yn in
+        [Yy]* ) 
+	    cd ;
+	    git clone git://github.com/JuliaLang/julia.git;
+	    cd julia;
+	    git checkout release-0.3;
+	    make ;
+	    break;;
+        [Nn]* ) sudo add-apt-repository ppa:staticfloat/juliareleases;
+	    sudo add-apt-repository ppa:staticfloat/julia-deps;
+	    sudo apt-get update;
+	    sudo apt-get install julia ;
+	    exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
