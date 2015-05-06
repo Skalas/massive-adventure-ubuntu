b27be0b713068c90c6df4e55fd97ad4dc72c493f
skalas <miguel@opi.la>
2015-05-06 12:50:50 -0500

    Agrego cosa que me tronó en servidores.

--- i_R.sh
+++ i_R.sh
@@ -5,14 +5,14 @@ while true; do
         [Yy]* ) sudo apt-get update;
 	    sudo apt-get upgrade -y;
 	    sudo apt-get install -y build-essential libpq-dev liblapack3 libblas3 libmysql++-dev sqlite3 fort77 gnuplot-x11 gfortran  texinfo liblapack-dev liblapack3gf texi2html libglpk-dev libgeos-dev libgdal1-dev libproj-dev;
-	    sudo apt-get -y build-dep r-base --no-install-recomends ;
+	    sudo apt-get -y build-dep r-base;
 	    cd;
 	    wget -c http://cran.r-project.org/src/base/R-latest.tar.gz;
 	    tar zxvf R-latest.tar.gz;
 	    cd "$(ls -dt R-*/ | head -1 )";
 	    ./configure --enable-memory-profiling --enable-R-shlib --with-blas --with-lapack --with-system-zlib --with-system-bzlib --with-system-xz --with-tcltk  --with-cairo --with-libpng --with-jpeglib --with-libtiff;
 	    make;
-	    sudo 	make install;
+	    sudo make install;
 	    break;;
         [Nn]* ) sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9;
 	    ubuntu_codename=`lsb_release -cs` ;
