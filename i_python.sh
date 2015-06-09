72c8929fcbfe4c334d4253df455b0d6790d73e33
skalas <miguel@opi.la>
2015-06-09 12:52:24 -0500

    Quito algunas librerías y limpio, cómo se escrie esto

--- i_python.sh
+++ i_python.sh
@@ -1,4 +1,4 @@
-echo 'y' | sudo apt-get install python-pip python-dev build-essential libboost-all-dev ipython-notebook python-tk
+sudo apt-get install -y python-pip python-dev build-essential libboost-all-dev 
 sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
 sudo pip install numpy
