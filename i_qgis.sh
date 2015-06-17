8ea73b02f282f0d9ee7c802d0db4db4651fa5ce3
skalas <miguel@opi.la>
2015-06-17 18:14:41 -0500

    Agrego instalador correcto de qgis

--- i_qgis.sh
+++ i_qgis.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 ubuntu_codename=`lsb_release -cs`
-echo "deb     http://qgis.org/debian $ubuntu_codename main" | sudo tee /etc/apt/sources.list.d/qgis.list
-echo "deb-src http://qgis.org/debian $ubuntu_codename main" | sudo tee /etc/apt/sources.list.d/qgis.list
-echo "deb     http://ppa.launchpad.net/ubuntugis/ubuntugis-unstable/ubuntu $ubuntu_codename main" | sudo tee /etc/apt/sources.list.d/qgis.list
+echo "deb     http://qgis.org/debian $ubuntu_codename main" | sudo tee  /etc/apt/sources.list.d/qgis.list
+echo "deb-src http://qgis.org/debian $ubuntu_codename main" | sudo tee -a /etc/apt/sources.list.d/qgis.list
+gpg --keyserver keyserver.ubuntu.com --recv DD45F6C3
+gpg --export --armor DD45F6C3 | sudo apt-key add -
 sudo apt-get -y update
 sudo apt-get -y upgrade
-#sudo apt-get -y install qgis python-qgis qgis-plugin-grass
+sudo apt-get -y install qgis python-qgis qgis-plugin-grass
