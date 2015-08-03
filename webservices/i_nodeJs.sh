3d734d1f563840f7c0a358743168f339c64e0ab2
skalas <miguel@opi.la>
2015-08-03 12:27:05 -0500

    Deprecated.

--- webservices/i_nodeJs.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update
-yes | sudo apt-get install python-software-properties python g++ make
-echo | sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-yes |sudo  apt-get install nodejs
