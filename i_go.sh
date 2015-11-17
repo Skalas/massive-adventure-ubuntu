ff396930d1b1db3a0d6e53bb252512994ccaef21
Miguel Escalante <escalas5@gmail.com>
2015-11-17 14:25:12 -0600

    fix

--- i_go.sh
+++ i_go.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 cd /tmp/
 wget https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
-sudo bash <<EOF
-tar -C /usr/local -xzf go1.5.1.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
-EOF
+sudo tar -C /usr/local -xzf go1.5.1.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+
