036780647822eea63bfec85d704b2d6de0b6435e
Miguel Escalante <escalas5@gmail.com>
2015-11-17 12:50:51 -0600

    wrong source

--- i_nvm.sh
+++ i_nvm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
 tail -3 ~/.bashrc >> ~/.zshrc
-source ~/.zshrc
+source ~/.bashrc
 nvm install v0.12.2
 nvm alias default v0.12.2
