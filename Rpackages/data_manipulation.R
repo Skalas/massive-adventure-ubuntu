4dfa470ead74ba1cc44fe3b3a063415aef123ecd
Miguel Escalante <escalas5@gmail.com>
2015-12-30 17:23:30 -0600

    new packages

--- Rpackages/data_manipulation.R
+++ Rpackages/data_manipulation.R
@@ -6,7 +6,10 @@ pacman::p_install_gh("hadley/readxl")
 pacman::p_load(plyr, dplyr, data.table, tidyr, stringr, lubridate, gsubfn, readr)
 
 # data visualization
-pacman::p_load(ggplot2, graphics, ggvis) 
+pacman::p_load(ggplot2, graphics, ggvis)
+pacman::p_install_gh("RcppCore/Rcpp")
+pacman::p_install_gh("rstats-db/DBI")
+pacman::p_install_gh("rstats-db/RPostgres")
 pacman::p_install_gh('ramnathv/htmlwidgets')
 pacman::p_install_gh('rstudio/leaflet')
 pacman::p_install_gh('bwlewis/rthreejs')
