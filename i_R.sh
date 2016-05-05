39a9e06834c8b11e0e98a8ad690cb090d7645283
Miguel <escalas5@gmail.com>
2016-05-05 12:31:55 -0500

    cambio para instalar R y luego borrarlo

--- i_R.sh
+++ i_R.sh
@@ -5,12 +5,12 @@ while true; do
         [Yy]* ) sudo apt-get update;
 	    sudo apt-get upgrade -y;
 	    sudo apt-get install -y build-essential libpq-dev liblapack3 libblas3 libmysql++-dev sqlite3 fort77 gnuplot-x11 gfortran  texinfo liblapack-dev liblapack3gf texi2html libglpk-dev libgeos-dev libgdal1-dev libproj-dev;
-	    sudo apt-get -y build-dep r-base;
+	    sudo apt-get install -y --no-install-recommends r-base r-base-dev;
 	    cd;
 	    wget -c http://cran.r-project.org/src/base/R-latest.tar.gz;
 	    tar zxvf R-latest.tar.gz;
 	    cd "$(ls -dt R-*/ | head -1 )";
-	    ./configure --enable-memory-profiling --enable-R-shlib --with-blas --with-lapack --with-system-zlib --with-system-bzlib --with-system-xz --with-tcltk  --with-cairo --with-libpng --with-jpeglib --with-libtiff;
+	    ./configure --enable-memory-profiling --enable-R-shlib --with-blas --with-lapack --with-system-zlib --with-system-bzlib --with-system-xz --with-tcltk  --with-cairo --with-libpng --with-jpeglib --with-libtiff --with-x=no;
 	    make;
 	    sudo make install;
 	    break;;
