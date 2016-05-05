11ba317ce79a7d7ba32ad0df00f7f35a8b856197
Miguel <escalas5@gmail.com>
2016-05-05 12:57:06 -0500

    delete?

--- i_R.sh
+++ i_R.sh
@@ -6,6 +6,7 @@ while true; do
 	    sudo apt-get upgrade -y;
 	    sudo apt-get install -y build-essential libpq-dev liblapack3 libblas3 libmysql++-dev sqlite3 fort77 gnuplot-x11 gfortran  texinfo liblapack-dev liblapack3gf texi2html libglpk-dev libgeos-dev libgdal1-dev libproj-dev;
 	    sudo apt-get install -y --no-install-recommends r-base r-base-dev;
+	    sudo apt-get remove -y r-base-core
 	    cd;
 	    wget -c http://cran.r-project.org/src/base/R-latest.tar.gz;
 	    tar zxvf R-latest.tar.gz;
