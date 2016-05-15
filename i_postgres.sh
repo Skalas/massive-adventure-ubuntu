4c16c7ec8b55549a045999769247f989f9b6f632
Miguel Escalante <escalas5@gmail.com>
2016-05-15 17:09:41 -0500

    Fixed version of ubuntu

--- i_postgres.sh
+++ i_postgres.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-ubuntu_codename=`lsb_release -cs` 
-echo "deb http://apt.postgresql.org/pub/repos/apt/ $ubuntu_codename-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get -y update
 sudo apt-get -y upgrade
