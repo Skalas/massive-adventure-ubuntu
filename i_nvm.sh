6b5dd5f0034732f81cab81c48a3445b6c61b148a
Miguel Escalante <escalas5@gmail.com>
2016-12-01 16:38:02 -0600

    update node version

--- i_nvm.sh
+++ i_nvm.sh
@@ -2,5 +2,5 @@
 curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
 tail -3 ~/.bashrc >> ~/.zshrc
 source ~/.zshrc
-nvm install v0.12.2
-nvm alias default v0.12.2
+nvm install v7.2.0 
+nvm alias default v7.2.0 
