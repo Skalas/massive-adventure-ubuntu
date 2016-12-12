b62ed63914346efa5d7c77f1123a80a43d1987c6
Miguel Escalante <escalas5@gmail.com>
2016-12-12 13:23:57 -0600

    Postgres Version update

--- i_postgres_client.sh
+++ i_postgres_client.sh
@@ -4,4 +4,4 @@ echo "deb http://apt.postgresql.org/pub/repos/apt/ $ubuntu_codename-pgdg main" |
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install postgresql-client-9.4
+sudo apt-get -y install postgresql-client-9.6
