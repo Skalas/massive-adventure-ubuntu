74e6f39645df1fe175987ec387b0b5a63fb49834
Miguel Escalante <escalas5@gmail.com>
2016-12-21 13:53:49 -0600

    mongo changes

--- i_mongo.sh
+++ i_mongo.sh
@@ -1,4 +1,20 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt update
 sudo apt install -y mongodb-org
+echo "[Unit]
+Description=High-performance, schema-free document-oriented database
+After=network.target
+Documentation=https://docs.mongodb.org/manual
+
+[Service]
+User=mongodb
+Group=mongodb
+ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /lib/systemd/system/mongod.service
+sudo systemctl daemon-reload
+systemctl start mongod
+systemctl enable mongod
