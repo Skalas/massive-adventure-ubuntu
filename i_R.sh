65732fb67237fecbed66d04fbf9f8f65e38d884f
Miguel Escalante <escalas5@gmail.com>
2017-02-16 17:35:53 -0600

    Src directory

--- i_R.sh
+++ i_R.sh
@@ -7,13 +7,15 @@ while true; do
 	    sudo apt-get install -y build-essential libpq-dev liblapack3 libblas3 libmysql++-dev sqlite3 fort77 gnuplot-x11  texinfo liblapack-dev texi2html libglpk-dev libgeos-dev libgdal1-dev libproj-dev;
 	    sudo apt install -y gfortran  autoconf automake bzip2-doc cdbs debhelper dh-strip-nondeterminism dh-translations gettext intltool intltool-debian libarchive-zip-perl libasprintf-dev libbz2-dev libfile-stripnondeterminism-perl libgettextpo-dev libgettextpo0 liblzma-dev libmail-sendmail-perl libncurses5-dev libpcre3-dev libpcre32-3 libpcrecpp0v5 libreadline-dev libreadline6-dev libsys-hostname-long-perl libtinfo-dev libunistring0 m4 po-debconf python-scour xorg-dev;
 	    sudo apt-get -y build-dep r-base;
-	    cd;
+	    mkdir -p $HOME/src;
+	    cd $HOME/src;
 	    wget -c http://cran.r-project.org/src/base/R-latest.tar.gz;
-	    tar zxvf R-latest.tar.gz;
+	    tar zxvf R-latest.tar.gz && rm R-latest.tar.gz;
 	    cd "$(ls -dt R-*/ | head -1 )";
 	    ./configure --enable-memory-profiling --enable-R-shlib --with-blas --with-lapack --with-tcltk  --with-cairo --with-libpng --with-jpeglib --with-libtiff;
 	    make;
 	    sudo make install;
+	    
 	    break;;
         [Nn]* ) sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9;
 	    ubuntu_codename=`lsb_release -cs` ;
