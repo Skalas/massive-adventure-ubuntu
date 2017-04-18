f15a3b69c48b0bcc5ed8fcbfb9914ad0b8071d3d
Miguel Escalante <escalas5@gmail.com>
2017-04-18 12:40:05 -0500

    fixed.

--- i_pyenv.sh
+++ i_pyenv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-echo 'export PATH="/home/skalas/.pyenv/bin:$PATH"' >> .zshrc
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> .zshrc
 echo 'eval "$(pyenv init -)"'  >> .zshrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> .zshrc
-echo 'export PATH="/home/skalas/.pyenv/bin:$PATH"' >> .bashrc
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> .bashrc
 echo 'eval "$(pyenv init -)"'  >> .bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> .bashrc
