5f5b269d25c636ea821f91bdc085c679513085aa
Miguel Escalante <escalas5@gmail.com>
2018-02-01 14:11:59 -0600

    wtf?¡

--- i_docky.sh
+++ i_docky.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo add-apt-repository -y ppa:ricotz/docky
 ## update and install 
-sudo apt-get update && sudo apt-get install -y cinnamon plank
+sudo apt-get update && sudo apt-get install -y docky
