25f7e1a0b5427bb9ca0516cd6d1b13713248734d
Miguel Escalante <escalas5@gmail.com>
2018-02-01 14:38:20 -0600

    dependencies

--- i_oh_my_zsh.sh
+++ i_oh_my_zsh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install -y zsh
+sudo apt install -y zsh tmux fortune
 curl -L git.io/antigen > ~/antigen.zsh
 cp config_files/zshrc ~/.zshrc
 cat config_files/env_vars >> ~/.env_vars
