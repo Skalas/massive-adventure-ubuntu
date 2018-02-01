f79bc8c05b0812cbb419e645a9c4749e03976185
Miguel Escalante <escalas5@gmail.com>
2018-02-01 15:45:40 -0600

    new keys

--- i_spotify.sh
+++ i_spotify.sh
@@ -1,4 +1,4 @@
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
 echo 'deb http://repository.spotify.com stable non-free' | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
 sudo apt-get install -y spotify-client
