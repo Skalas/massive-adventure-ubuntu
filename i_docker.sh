7af46d272b9040f51e007302ff6834e59f783474
Miguel Escalante <escalas5@gmail.com>
2018-07-16 14:02:15 -0500

    changes to docker install

--- i_docker.sh
+++ i_docker.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 ## We check that we can manage this
-ubuntu_codename=`lsb_release -cs`
 sudo bash <<EOF
+## Prerequisites
+apt-get install -y 
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
 ## We get the public keys
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 ## We add the repository to the sources
-echo "deb https://apt.dockerproject.org/repo ubuntu-$ubuntu_codename main" > /etc/apt/sources.list.d/docker.list
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 apt-get update
-apt-get install -y docker-engine
+apt-get install -y docker-ce
 EOF
 sudo gpasswd -a ${USER} docker
 sudo service docker restart
