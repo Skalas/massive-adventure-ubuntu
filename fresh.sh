51ea89a70c9ef4433397e64bb259c80c5c24e07f
Miguel Angel Escalante <Skalas@users.noreply.github.com>
2018-09-17 23:26:34 -0500

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -1,2 +1,3 @@
-sudo apt-get install -y git curl libcurl4-openssl-dev build-essential
+sudo apt-get install -y git curl libcurl4-openssl-dev build-essential python-pip
 mkdir -p $HOME/src
+mkdir -p $HOME/github
