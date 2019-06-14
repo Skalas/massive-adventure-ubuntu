dd4b40efa74e75853cbdd6bbd53afc36196d9e95
Miguel Escalante <miguel.escalante@globant.com>
2019-06-14 16:51:37 -0500

    update postgresql version

--- i_postgres.sh
+++ i_postgres.sh
@@ -3,7 +3,7 @@ echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" | sudo tee 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install postgresql-9.6
+sudo apt-get -y install postgresql-9.11
 usr=$USER
 sudo -u postgres bash <<EOF
 createuser "$usr"
