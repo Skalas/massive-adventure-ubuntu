800bb41bb497c7dc4dd7fe22a511a5b81a44df82
Miguel Escalante <miguel.escalante@globant.com>
2019-06-14 18:30:43 -0500

    mv to src folder

--- i_oh_my_zsh.sh
+++ i_oh_my_zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt install -y zsh tmux fortune
-curl -L git.io/antigen > ~/antigen.zsh
+curl -L git.io/antigen > ~/src/antigen.zsh
 cp config_files/zshrc ~/.zshrc
 cat config_files/env_vars >> ~/.env_vars
 cp config_files/aliases ~/aliases
