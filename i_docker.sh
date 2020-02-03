7cb3ec8e3470292261c8321bc4b8970221cb9928
Miguel Escalante <escalas5@gmail.com>
2020-02-03 17:29:03 -0600

    Temporary release fix

--- i_docker.sh
+++ i_docker.sh
@@ -6,16 +6,18 @@ apt install -y \
     apt-transport-https \
     ca-certificates \
     curl \
+    gnupg-agent \
     software-properties-common
+
 ## We get the public keys
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 ## We add the repository to the sources
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
+   disco \
    stable"
 apt update
-apt install -y docker-ce
+apt install -y docker-ce docker-ce-cli containerd.io
 EOF
 sudo gpasswd -a ${USER} docker
 sudo service docker restart
