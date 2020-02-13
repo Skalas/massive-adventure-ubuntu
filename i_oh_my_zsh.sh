5fe411ecb155038c07d7fa68614ac22232b62e15
Miguel Escalante <escalas5@gmail.com>
2020-02-13 01:18:40 -0600

    change the path where it goes

--- i_oh_my_zsh.sh
+++ i_oh_my_zsh.sh
@@ -3,5 +3,5 @@ sudo apt install -y zsh tmux fortune
 curl -L git.io/antigen > ~/src/antigen.zsh
 cp config_files/zshrc ~/.zshrc
 cat config_files/env_vars >> ~/.env_vars
-cp config_files/aliases ~/aliases
+cp config_files/aliases ~/src/aliases
 chsh -s /bin/zsh
