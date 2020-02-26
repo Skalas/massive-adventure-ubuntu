d448e006ba2dc49a87fc3b50e96f652d68abbca6
Miguel Escalante <escalas5@gmail.com>
2020-02-26 12:44:04 -0600

    new files to zshrc

--- i_oh_my_zsh.sh
+++ i_oh_my_zsh.sh
@@ -2,6 +2,7 @@
 sudo apt install -y zsh tmux fortune
 curl -L git.io/antigen > ~/src/antigen.zsh
 cp config_files/zshrc ~/.zshrc
+cp config_files/anaconda ~/src/load_anaconda.zsh
 cat config_files/env_vars >> ~/.env_vars
 cp config_files/aliases ~/src/aliases
 chsh -s /bin/zsh
