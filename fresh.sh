0032edd6af4638bddaebba761254d46927957423
Miguel Escalante <escalas5@gmail.com>
2022-08-15 15:49:25 -0500

    add xclip

--- fresh.sh
+++ fresh.sh
@@ -1,3 +1,3 @@
-sudo apt install -y git curl libcurl4-openssl-dev build-essential python3-pip stow libxml2-dev fonts-firacode fonts-cantarell
+sudo apt install -y git curl libcurl4-openssl-dev build-essential python3-pip stow libxml2-dev fonts-firacode fonts-cantarell xclip
 mkdir -p $HOME/src
 mkdir -p $HOME/github
